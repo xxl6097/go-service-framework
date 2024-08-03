0a60d38369bc40922680448719d62b612759aaa7
MacBookAirM2 <uuxia@macbookairm2.com>
2024-08-03 11:31:29 +0800

    release 0.2.72

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -16,9 +16,32 @@ func init() {
 //go:generate goversioninfo -icon=resource/icon.ico -manifest=resource/goversioninfo.exe.manifest
 func main() {
 	test()
-	//glog.SetLogFile("./", "install.log")
 	gservice.Run(&framework.Framework{})
 }
 
 func test() {
+	//binDir := "/Users/uuxia/Downloads/sun-panel"
+	//binPath := "/Users/uuxia/Downloads/sun-panel/sun-panel.zip"
+	//isZip := zip.IsZip(binPath)
+	//if isZip {
+	//	//确定解压成功
+	//	zipDir, err := zip.GetRootDir(binDir)
+	//	if err == nil && zipDir != "" {
+	//		//确定zip有一级目录
+	//		binDir = filepath.Join(binDir, zipDir)
+	//	}
+	//	fileName := "sun-panel"
+	//	if zipDir != "" {
+	//		fileName = zipDir
+	//	}
+	//	file.ScanDirectoryAndFunc(binDir, func(fName string) {
+	//		if strings.HasPrefix(strings.ToLower(fileName), strings.ToLower(fName)) {
+	//			binFilePath := filepath.Join(binDir, fName)
+	//			executable, err := os1.IsExecutable(binFilePath)
+	//			if err == nil && executable {
+	//				binPath = binFilePath
+	//			}
+	//		}
+	//	})
+	//}
 }
